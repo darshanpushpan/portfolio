a4261018bb206962763f59b4a0314785217c675e
Darshan Pushpangadhan <71029227+darshanpushpan@users.noreply.github.com>
2024-11-06 01:08:31 -0500

    Update app.js
    Back to old version

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll('nav a[href^="#"]');
-    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
-    const mobileNav = document.getElementById('mobile-nav');
-
-    // Smooth scrolling and background color effect on click
     links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
-
             const targetId = this.getAttribute('href');
             const targetElement = document.querySelector(targetId);
-
             if (targetElement) {
                 targetElement.style.transition = 'background-color 0.3s ease-in-out';
                 targetElement.style.backgroundColor = 'none';
-
                 targetElement.scrollIntoView({
                     behavior: 'smooth',
                     block: 'start'
                 });
-
                 setTimeout(() => {
                     targetElement.style.backgroundColor = '';
                 }, 300);
@@ -28,22 +20,4 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-
-    // Toggle the mobile menu
-    mobileMenuToggle.addEventListener('click', toggleMenu);
-
-    function toggleMenu() {
-        mobileNav.classList.toggle('show');
-
-        // Optional: Show an overlay when the menu is open
-        if (mobileNav.classList.contains('show')) {
-            document.body.insertAdjacentHTML(
-                'beforeend',
-                '<div class="overlay" onclick="toggleMenu()"></div>'
-            );
-        } else {
-            const overlay = document.querySelector('.overlay');
-            if (overlay) overlay.remove();
-        }
-    }
 });
