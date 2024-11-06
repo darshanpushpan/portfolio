e5a28562a757b38af0bf2bafbaebde6b2dbb288a
Darshan Pushpangadhan <71029227+darshanpushpan@users.noreply.github.com>
2024-11-06 00:59:55 -0500

    Update app.js
    Update 1

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll('nav a[href^="#"]');
+    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+    const mobileNav = document.getElementById('mobile-nav');
 
+    // Smooth scrolling and background color effect on click
     links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
@@ -25,4 +28,22 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    // Toggle the mobile menu
+    mobileMenuToggle.addEventListener('click', toggleMenu);
+
+    function toggleMenu() {
+        mobileNav.classList.toggle('show');
+
+        // Optional: Show an overlay when the menu is open
+        if (mobileNav.classList.contains('show')) {
+            document.body.insertAdjacentHTML(
+                'beforeend',
+                '<div class="overlay" onclick="toggleMenu()"></div>'
+            );
+        } else {
+            const overlay = document.querySelector('.overlay');
+            if (overlay) overlay.remove();
+        }
+    }
 });
