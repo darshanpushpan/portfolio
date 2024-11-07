6aea051b57cd87a30d89ab8a15d3209db5071ca4
Darshan Pushpangadhan <71029227+darshanpushpan@users.noreply.github.com>
2024-11-07 18:56:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll('nav a[href^="#"]');
+    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+    const sideMenu = document.getElementById('side-menu');
+    const menuLinks = document.querySelectorAll('#side-menu a');
+
+    // Toggle the mobile side menu when the hamburger icon is clicked
+    mobileMenuToggle.addEventListener('click', function () {
+        sideMenu.classList.toggle('show');
+    });
+
+    // Close the side menu when a menu link is clicked
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            sideMenu.classList.remove('show');
+        });
+    });
+
+    // Smooth scrolling for anchor links
+    const links = document.querySelectorAll('a[href^="#"]');
     links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
             const targetId = this.getAttribute('href');
             const targetElement = document.querySelector(targetId);
             if (targetElement) {
-                targetElement.style.transition = 'background-color 0.3s ease-in-out';
-                targetElement.style.backgroundColor = 'none';
                 targetElement.scrollIntoView({
                     behavior: 'smooth',
                     block: 'start'
                 });
-                setTimeout(() => {
-                    targetElement.style.backgroundColor = '';
-                }, 300);
             } else {
                 console.error("Target element not found for ID:", targetId);
             }
